d806efd44ae75c11505817617fe56048d24ffaf8
Isa saikiran <88225022+saikiranisa@users.noreply.github.com>
2022-07-18 15:53:49 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
